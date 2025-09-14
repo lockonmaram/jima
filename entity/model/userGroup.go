393f259e987f67fe7975ecc852bc5e6c9fc2163d
lockonmaram <maramstratos@gmail.com>
2025-09-14 15:25:58 +0700

    add user group role to  table struct

--- entity/model/userGroup.go
+++ entity/model/userGroup.go
@@ -12,12 +12,13 @@ const (
 )
 
 type UserGroup struct {
-	Serial      string     `db:"serial" json:"serial"`
-	UserSerial  string     `db:"user_serial" json:"user_serial"`
-	GroupSerial string     `db:"group_serial" json:"group_serial"`
-	CreatedAt   time.Time  `db:"created_at" json:"created_at"`
-	UpdatedAt   *time.Time `db:"updated_at" json:"updated_at"`
-	DeletedAt   *time.Time `db:"deleted_at" json:"deleted_at"`
+	Serial      string        `db:"serial" json:"serial"`
+	UserSerial  string        `db:"user_serial" json:"user_serial"`
+	GroupSerial string        `db:"group_serial" json:"group_serial"`
+	Role        UserGroupRole `db:"role" json:"role"`
+	CreatedAt   time.Time     `db:"created_at" json:"created_at"`
+	UpdatedAt   *time.Time    `db:"updated_at" json:"updated_at"`
+	DeletedAt   *time.Time    `db:"deleted_at" json:"deleted_at"`
 }
 
 func (UserGroup) TableName() string {
